c7a1190483fd8ad65ff157e1939575e9aeb01002
yy <2352798247@qq.com>
2022-09-24 17:55:02 +0800

    首页功能

--- main.js
+++ main.js
@@ -2,7 +2,27 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
 
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+$http.baseUrl = "https://api-hmugo-web.itheima.net"
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+uni.$showMsg = function(title = '数据请求失败！',duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,37 @@
 <template>
   <view>
-    home
+    <swiper :indicator-dots="true" autoplay="true" :interval="3000" :circular="true">
+      <swiper-item v-for="(item,index) in swiperList" :key="index">
+        <navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
+          <image :src="item.image_src" ></image>
+        </navigator> 
+      </swiper-item>
+    </swiper>
+<view class="nav-list">
+   <view class="nav-item" v-for="(item, index) in navList" :key="index" @click="navClickHandler(item)">
+     <image :src="item.image_src" class="nav-img"></image>
+   </view>
+</view>
+<view class="floor-list">
+  <!-- 楼层 item 项 -->
+  <view class="floor-item" v-for="(item, index) in floorList" :key="index">
+    <!-- 楼层标题 -->
+    <image :src="item.floor_title.image_src" class="floor-title"></image>
+    <!-- 楼层图片区域 -->
+<view class="floor-img-box">
+  <!-- 左侧大图片的盒子 -->
+  <navigator class="left-img-box" :url="item.product_list[0].url">
+    <image :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+  </navigator>
+  <!-- 右侧 4 个小图片的盒子 -->
+  <view class="right-img-box">
+    <navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0" :url="item2.url">
+      <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
+    </navigator>
+  </view>
+</view>
+  </view>
+</view>
   </view>
 </template>
 
@@ -8,12 +39,86 @@
   export default {
     data() {
       return {
-        
+        swiperList: [],
+        navList:[],
+        floorList:[]
       };
+    },
+    onLoad(){
+      this.getSwiperList()
+      this.getNavList()
+      this.getFloorList()
+    },
+    methods:{
+      async getSwiperList(){
+        let res = await uni.$http.get('/api/public/v1/home/swiperdata')
+       if(res.data.meta.status !== 200) {
+         return uni.$showMsg()
+       } else {
+         this.swiperList = res.data.message
+       }
+      },
+      async getNavList(){
+        let res = await uni.$http.get('/api/public/v1/home/catitems')
+       if(res.data.meta.status !== 200) {
+         return uni.$showMsg()
+       } else {
+         this.navList = res.data.message
+       }
+      },
+      async getFloorList() {
+            const { data: res } = await uni.$http.get('/api/public/v1/home/floordata')
+            if (res.meta.status !== 200) return uni.$showMsg()
+             res.message.forEach(floor => {
+    floor.product_list.forEach(prod => {
+      prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+    })
+  })
+            this.floorList = res.message
+       },
+      navClickHandler(item) {
+        // 判断点击的是哪个 nav
+        if (item.name === '分类') {
+          uni.switchTab({
+            url: "/pages/category/category"
+          })
+        }
+      }
     }
   }
 </script>
 
 <style lang="scss">
+swiper{
+  height: 330rpx;
+  .swiper-item ,image{
+    width: 100%;
+    height: 100%;
+  }
+}
+.nav-list {
+  display: flex;
+  justify-content: space-around;
+  margin: 15px 0;
 
+  .nav-img {
+    width: 128rpx;
+    height: 140rpx;
+  }
+}
+.floor-title {
+  height: 60rpx;
+  width: 100%;
+  display: flex;
+}
+.right-img-box {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.floor-img-box {
+  display: flex;
+  padding-left: 10rpx;
+}
 </style>
