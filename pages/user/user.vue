bf35eeff4e2fcef1f6236486ab370950b66fa0d5
yy <2352798247@qq.com>
2022-09-30 18:50:15 +0800

    支付

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,19 +1,30 @@
 <template>
-  <view>
-    
+  <view class="my-container">
+    <login v-if="!token"></login>
+    <userinfo v-else></userinfo>
   </view>
 </template>
 
 <script>
+ import badgeMix from '@/mixins/tabbar-badge.js'
+ import {mapState} from 'vuex'
   export default {
+    mixins:[badgeMix],
     data() {
       return {
         
       };
-    }
+    },
+      computed: {
+        // 2. 从 m_user 模块中导入需要的 token 字符串
+        ...mapState('user', ['token']),
+      },
   }
 </script>
 
 <style lang="scss">
-
+page,
+.my-container {
+  height: 100%;
+}
 </style>
